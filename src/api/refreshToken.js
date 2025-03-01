d17d782b70a78730e1c588c7bd3e3cd18894b4d8
jjwhite224 <56843367+jjwhite224@users.noreply.github.com>
2025-02-28 20:12:14 -0500

    Update refreshToken.js

--- src/api/refreshToken.js
+++ src/api/refreshToken.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = "http://localhost:3001";
+const BACKEND_URL = "https://spotbubbles.onrender.com";
 
 export const refreshAccessToken = async () => {
   const refreshToken = localStorage.getItem("spotify_refresh_token");
